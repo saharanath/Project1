761e47891767b4fd9ba536d679203884616eae11
Heng <bunsanphe@gmail.com>
2020-05-14 19:25:18 -0700

    updated buttons

--- Assets/Script/script.js
+++ Assets/Script/script.js
@@ -1,66 +1,86 @@
 // 
+var maxDistance;
+var minLength;
+var minStars;
+var apiKey = "key=200749192-819757ad274cc592a221c4c70b9c441e";
+var i = 0;
+var trails;
+var setLocation;
 
-navigator.geolocation.getCurrentPosition(function (location) {
-
-    var maxDistance;
-    var minLength;
-    var minStars;
-    var apiKey = "key=200749192-819757ad274cc592a221c4c70b9c441e";
-    var i = 0;
-    var trails;
-    $("button").on("click", getTrails);
-
-    function getTrails(event) {
-        maxDistance = $(".maxDistance").val();
-        minLength = $(".minLength").val();
-console.log(location.coords.longitude);
-        console.log($(".minStar").val());
-        console.log(maxDistance);
-        console.log(minLength);
-        var queryURL = "https://www.hikingproject.com/data/get-trails?" +
-            "lat=" + location.coords.latitude + "&lon=" + location.coords.longitude +
-            "&maxDistance=" + maxDistance + "&minLength=" + minLength + "&minStars=" + minStars + "&" + apiKey;
-
-        event.preventDefault();
-        $.ajax({
-                url: queryURL,
-                method: "GET"
-            })
-            .then(function (response) {
-                trails = response.trails
-                console.log(queryURL)
-                $(".name").html("Trail Name: " + response.trails[i].name);
-                $(".location").html("Location: " + response.trails[i].location);
-                $(".ascent").text("Ascent: " + response.trails[i].ascent + "ft");
-                $(".descent").text("Descent: " + response.trails[i].descent + "ft");
-                $(".length").text("Length: " + response.trails[i].length + "mi");
-                $(".stars").text("Stars: " + response.trails[i].stars);
-                parseDifficulty(response.trails[i].difficulty);
-
-                i++;
-                if (i === trails.length) {
-                    i = 0
-                }
-
-                fetchWeather(response.trails[i].location);
-            });
-    }
-
-    function parseDifficulty(difficulty) {
-        var difficultyLevel;
-        if (difficulty === "green") {
-            difficultyLevel = "Easy";
-        }
-        if (difficulty === "blue") {
-            difficultyLevel = "Intermediate";
-        }
-        if (difficulty === "black") {
-            difficultyLevel = "Hard";
-        }
-        $(".difficulty").text("Difficulty: " + difficultyLevel);
-
-    }
+navigator.geolocation.getCurrentPosition(function (currentLocation) {
+    setLocation = currentLocation;
 })
+
+
+// $("button").on("click", getTrails);
+
+function getTrails(i) {
+    maxDistance = $(".maxDistance").val();
+    minLength = $(".minLength").val();
+    console.log(setLocation.coords.longitude);
+    console.log($(".minStar").val());
+    console.log(maxDistance);
+    console.log(minLength);
+    var queryURL = "https://www.hikingproject.com/data/get-trails?" +
+        "lat=" + setLocation.coords.latitude + "&lon=" + setLocation.coords.longitude +
+        "&maxDistance=" + maxDistance + "&minLength=" + minLength + "&minStars=" + minStars + "&" + apiKey;
+
+    // event.preventDefault();
+    $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        .then(function (response) {
+            trails = response.trails
+            console.log(response)
+            console.log(queryURL)
+            $(".name").html("Trail Name: " + response.trails[i].name);
+            $(".location").html("Location: " + response.trails[i].location);
+            $(".ascent").text("Ascent: " + response.trails[i].ascent + "ft");
+            $(".descent").text("Descent: " + response.trails[i].descent + "ft");
+            $(".length").text("Length: " + response.trails[i].length + "mi");
+            $(".stars").text("Stars: " + response.trails[i].stars);
+            parseDifficulty(response.trails[i].difficulty);
+
+            // i++;
+            // if (i === trails.length) {
+            //     i = 0
+            // }
+
+            fetchWeather(response.trails[i].location);
+        });
+}
+
+function parseDifficulty(difficulty) {
+    var difficultyLevel;
+    if (difficulty === "green") {
+        difficultyLevel = "Easy";
+    }
+    if (difficulty === "blue") {
+        difficultyLevel = "Intermediate";
+    }
+    if (difficulty === "black") {
+        difficultyLevel = "Hard";
+    }
+    $(".difficulty").text("Difficulty: " + difficultyLevel);
+
+}
+
+
+$("#submitButton").on("click", function(params){
+    params.preventDefault()
+    var startIndex = 0;
+    getTrails(startIndex);
+})
+$(".nextButton").on("click", function(params){
+    params.preventDefault();
+    var increaseIndex = i++
+    if (i >= trails.length) {
+        i = 0
+    }
+    getTrails(increaseIndex);
+})    
+
 
 function fetchWeather(city) {
     var city;
