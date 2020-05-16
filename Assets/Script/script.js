a0f727b6de65ad22ad0b3aa8d54c21c7acc063ab
Sahara Nath <saharanath@hotmail.com>
2020-05-16 10:35:05 -0700

    Comments/console log

--- Assets/Script/script.js
+++ Assets/Script/script.js
@@ -18,31 +18,25 @@ navigator.geolocation.getCurrentPosition(function (currentLocation) {
             }
         }
     })
-//     https://www.hikingproject.com/data/get-trails?lat=47.68399360000001&lon=-122.2148096&maxDistance=&minLength=&minStars=1&key=200749192-819757ad274cc592a221c4c70b9c441e
-//     https://www.hikingproject.com/data/get-trails?lat=122.3321&lon=47.6062&maxDistance=12&minLength=1&minStars=1&key=200749192-819757ad274cc592a221c4c70b9c441e
-// // $("button").on("click", getTrails);
+
 
 function getTrails(i) {
     maxDistance = $(".maxDistance").val();
     minLength = $(".minLength").val();
     minStars = $("minStar").children("option:selected").val();
-    console.log(setLocation.coords.longitude);
-    console.log($(".minStar").val());
-    console.log(maxDistance);
-    console.log(minLength);
+    
     var queryURL = "https://www.hikingproject.com/data/get-trails?" +
         "lat=" + setLocation.coords.latitude + "&lon=" + setLocation.coords.longitude +
         "&maxDistance=" + maxDistance + "&minLength=" + minLength + "&minStars=" + minStars + "&" + apiKey;
 
-    // event.preventDefault();
+   
     $.ajax({
             url: queryURL,
             method: "GET"
         })
         .then(function (response) {
             trails = response.trails
-            console.log(response)
-            console.log(queryURL)
+            
             $(".name").html("Trail Name: " + response.trails[i].name);
             $(".location").html("Location: " + response.trails[i].location);
             $(".ascent").text("Ascent: " + response.trails[i].ascent + "ft");
@@ -51,10 +45,6 @@ function getTrails(i) {
             $(".stars").text("Stars: " + response.trails[i].stars);
             parseDifficulty(response.trails[i].difficulty);
 
-            // i++;
-            // if (i === trails.length) {
-            //     i = 0
-            // }
 
             fetchWeather(response.trails[i].location);
         });
@@ -98,8 +88,7 @@ function fetchWeather(city) {
     var cityAPIUrl = ("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey);
 
 
-    // var weatherQueryURL = ("https://api.openweathermap.org/data/2.5/onecall?lat=" + location.coords.latitude + "&lon=" + location.coords.longitude + "&units=imperial&exclude=hourly&appid=" + apiKey);
-
+    
     $.ajax({
         url: cityAPIUrl,
         method: "GET"
@@ -121,41 +110,3 @@ function fetchWeather(city) {
     })
 
 }
-// });
-// // var lon= geolocation.getCurrentPosition();
-// // var lat= position.coords.latitude;
-
-// //*7 days
-// var extendedForecastTemp1 = response.daily[1].temp.max;
-// console.log(extendedForecastTemp1)
-// var extendedForecastWeatherDescription1 = response.daily[1].weather[0].description;
-// // $('selector').text(str.charAt(0).toUpperCase() + str.substr(1).toLowerCase())
-// console.log(extendedForecastWeatherDescription1)
-
-// var extendedForecastTemp2 = response.daily[2].temp.max;
-// var extendedForecastWeatherDescription2 = response.daily[2].weather[0].description;
-// var extendedForecastTemp3 = response.daily[3].temp.max;
-// var extendedForecastWeatherDescription3 = response.daily[3].weather[0].description;
-// var extendedForecastTemp4 = response.daily[4].temp.max;
-// var extendedForecastWeatherDescription4 = response.daily[4].weather[0].description;
-// var extendedForecastTemp5 = response.daily[5].temp.max;
-// var extendedForecastWeatherDescription5 = response.daily[5].weather[0].description;
-// var extendedForecastTemp6 = response.daily[6].temp.max;
-// var extendedForecastWeatherDescription6 = response.daily[6].weather[0].description;
-// var extendedForecastTemp7 = response.daily[7].temp.max;
-// var extendedForecastWeatherDescription7 = response.daily[7].weather[0].description;
-
-// $(".extendedForecastTemp").append(extendedForecastTemp1)
-// $(".extendedForecastDescription").append(extendedForecastWeatherDescription1)
-// $(".extendedForecastTemp").append(extendedForecastTemp2)
-// $(".extendedForecastDescription").append(extendedForecastWeatherDescription2)
-// $(".extendedForecastTemp").append(extendedForecastTemp3)
-// $(".extendedForecastDescription").append(extendedForecastWeatherDescription3)
-// $(".extendedForecastTemp").append(extendedForecastTemp4)
-// $(".extendedForecastDescription").append(extendedForecastWeatherDescription4)
-// $(".extendedForecastTemp").append(extendedForecastTemp5)
-// $(".extendedForecastDescription").append(extendedForecastWeatherDescription5)
-// $(".extendedForecastTemp").append(extendedForecastTemp6)
-// $(".extendedForecastDescription").append(extendedForecastWeatherDescription6)
-// $(".extendedForecastTemp").append(extendedForecastTemp7)
-// $(".extendedForecastDescription").append(extendedForecastWeatherDescription7)
